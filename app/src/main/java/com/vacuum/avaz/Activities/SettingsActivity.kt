f720e969a6885a64de8d6ac91ce8f432f924b13f
mohamed ebrahim <ebrahimm131@gmail.com>
2018-07-25 15:26:58 +0200

    fix all theme bugs

--- app/src/main/java/com/vacuum/avaz/Activities/SettingsActivity.kt
+++ app/src/main/java/com/vacuum/avaz/Activities/SettingsActivity.kt
@@ -2,22 +2,19 @@ package com.vacuum.avaz.Activities
 
 import android.content.Context
 import android.widget.AdapterView
-import com.vacuum.avaz.Utils.ThemeApplication
 import android.widget.Spinner
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.vacuum.avaz.ContextActivity
-import com.vacuum.avaz.MainActivity
 import com.vacuum.avaz.R
 import com.vacuum.avaz.Utils.Utility
-import android.content.SharedPreferences
 
 
 
 class SettingsActivity : ContextActivity() {
     private var spThemes: Spinner? = null
-    val MY_PREFS_NAME = "MyPrefsFile"
+    val MY_PREFS_NAME = "avaz"
+    var sTheme =0
 
     // Here we set the theme for the activity
     // Note `Utils.onActivityCreateSetTheme` must be called before `setContentView`
@@ -32,17 +29,21 @@ class SettingsActivity : ContextActivity() {
 
     private fun setupSpinnerItemSelection() {
         spThemes = findViewById(R.id.spThemes) as Spinner
-        spThemes!!.setSelection(ThemeApplication.currentPosition)
-        ThemeApplication.currentPosition = spThemes!!.selectedItemPosition
+        val prefs = getSharedPreferences(MY_PREFS_NAME, Context.MODE_PRIVATE)
+        sTheme = prefs.getInt("theme", 0)
+        spThemes!!.setSelection(sTheme)
+        sTheme = spThemes!!.selectedItemPosition
 
         spThemes!!.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 
             override fun onItemSelected(parent: AdapterView<*>, view: View,
                                         position: Int, id: Long) {
-                Utility().changeToTheme(this@SettingsActivity, 1)
+                if (sTheme != position) {
+                Utility().changeToTheme(this@SettingsActivity, position)
+                }
+                sTheme = position
                 val editor = getSharedPreferences(MY_PREFS_NAME, Context.MODE_PRIVATE).edit()
-                editor.putString("name", "Elena")
-                editor.putInt("idName", 12)
+                editor.putInt("theme", position)
                 editor.apply()
             }
 
--- app/src/main/java/com/vacuum/avaz/Utils/Utility.kt
+++ app/src/main/java/com/vacuum/avaz/Utils/Utility.kt
@@ -1,13 +1,15 @@
 package com.vacuum.avaz.Utils
 import android.app.Activity
 import android.content.Intent
-import com.vacuum.avaz.Activities.SettingsActivity
 import com.vacuum.avaz.R
+import android.content.Context.MODE_PRIVATE
+import com.vacuum.avaz.MainActivity
 
 
 class Utility
 {
     private var sTheme: Int = 0
+    val MY_PREFS_NAME = "avaz"
 
     val Light_Theme = 0
     val Dark_Theme = 1
@@ -16,12 +18,14 @@ class Utility
     fun changeToTheme(activity: Activity, theme: Int) {
         sTheme = theme
         activity.finish()
-        activity.startActivity(Intent(activity, SettingsActivity::class.java))
+        activity.startActivity(Intent(activity, MainActivity::class.java))
         activity.overridePendingTransition(android.R.anim.fade_in,
                 android.R.anim.fade_out)
     }
 
     fun onActivityCreateSetTheme(activity: Activity) {
+        val prefs = activity.getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE)
+        sTheme = prefs.getInt("theme", 0)
         when (sTheme) {
             Light_Theme -> activity.setTheme(R.style.Light_Theme)
             Dark_Theme -> activity.setTheme(R.style.Dark_Theme)
