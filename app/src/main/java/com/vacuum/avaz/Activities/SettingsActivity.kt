fc5e92b8fc732043f4fce636b572e3fc5c4212c5
mohamed ebrahim <ebrahimm131@gmail.com>
2018-07-27 12:11:48 +0200

    add new languages / fix bugs

--- app/src/main/java/com/vacuum/avaz/Activities/SettingsActivity.kt
+++ app/src/main/java/com/vacuum/avaz/Activities/SettingsActivity.kt
@@ -8,13 +8,19 @@ import android.view.View
 import com.vacuum.avaz.ContextActivity
 import com.vacuum.avaz.R
 import com.vacuum.avaz.Utils.Utility
-
+import android.content.Intent
+import android.util.DisplayMetrics
+import java.util.*
 
 
 class SettingsActivity : ContextActivity() {
     private var spThemes: Spinner? = null
+    private var splanguages: Spinner? = null
+
     val MY_PREFS_NAME = "avaz"
     var sTheme =0
+    var myLocale:Locale?=null
+    var slangage:Int?=null
 
     // Here we set the theme for the activity
     // Note `Utils.onActivityCreateSetTheme` must be called before `setContentView`
@@ -25,6 +31,22 @@ class SettingsActivity : ContextActivity() {
         // AFTER SETTING THEME
         setContentView(R.layout.activity_settings)
         setupSpinnerItemSelection()
+        setupSpinner2()
+    }
+
+    private fun setupSpinner2() {
+        splanguages = findViewById(R.id.splanguages) as Spinner
+        val prefs = getSharedPreferences(MY_PREFS_NAME, Context.MODE_PRIVATE)
+        slangage = prefs.getInt("language", 0)
+        splanguages!!.setSelection(slangage!!)
+        splanguages!!.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View,
+                                        position: Int, id: Long) {
+
+                setLocale(position)
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
     }
 
     private fun setupSpinnerItemSelection() {
@@ -46,5 +68,24 @@ class SettingsActivity : ContextActivity() {
             }
             override fun onNothingSelected(parent: AdapterView<*>) {}
         }
+    }
+
+    fun setLocale(lang: String) {
+        val editor = getSharedPreferences(MY_PREFS_NAME, Context.MODE_PRIVATE).edit()
+        editor.putString("language", lang)
+        editor.apply()
+        when(lang){
+            0 -> myLocale = Locale("en")
+            1 -> myLocale = Locale("es")
+            2 -> myLocale = Locale("fr")
+
+        }
+        val res = resources
+        val dm = res.displayMetrics
+        val conf = res.configuration
+        conf.locale = myLocale
+        res.updateConfiguration(conf, dm)
+        val refresh = Intent(this, SettingsActivity::class.java)
+        startActivity(refresh)
     }
 }
--- app/src/main/java/com/vacuum/avaz/MainActivity.kt
+++ app/src/main/java/com/vacuum/avaz/MainActivity.kt
@@ -17,6 +17,9 @@ import android.widget.Toast
 import com.vacuum.avaz.Activities.SettingsActivity
 import com.vacuum.avaz.Utils.Utility
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
+import android.util.DisplayMetrics
+import java.util.*
+
 
 class MainActivity : ContextActivity()  , NavigationView.OnNavigationItemSelectedListener {
     private lateinit var drawer: DrawerLayout
@@ -25,10 +28,20 @@ class MainActivity : ContextActivity()  , NavigationView.OnNavigationItemSelecte
     private lateinit var pagerAdapter: MyPagerAdapter
     private lateinit var viewpager_main: ViewPager
     private lateinit var mContext: Context
+    private val MY_PREFS_NAME = "avaz"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Utility().onActivityCreateSetTheme(this)
+        mContext = this
+        val prefs = getSharedPreferences(MY_PREFS_NAME, Context.MODE_PRIVATE)
+        val slangage = prefs.getString("language", "en")
+        val res = mContext.getResources()
+        val dm = res.getDisplayMetrics()
+        val conf = res.getConfiguration()
+        conf.setLocale(Locale(slangage))
+         // API 17+ only.
+        res.updateConfiguration(conf, dm)
         setContentView(R.layout.activity_main)
 
         val toolbar: Toolbar = findViewById(R.id.toolbar_main)
@@ -37,7 +50,6 @@ class MainActivity : ContextActivity()  , NavigationView.OnNavigationItemSelecte
         drawer = findViewById(R.id.drawer_layout)
         val navigationView: NavigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
-        mContext = this
 
         pagerAdapter = MyPagerAdapter(supportFragmentManager,mContext)
         viewpager_main = findViewById(R.id.viewpager_main)
@@ -69,13 +81,18 @@ class MainActivity : ContextActivity()  , NavigationView.OnNavigationItemSelecte
     }
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.nav_item_one ->{
+            R.id.top_stories ->{
                 Toast.makeText(this, "Clicked item one", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, SettingsActivity::class.java)
                     startActivity(intent) }
-            R.id.nav_item_two -> Toast.makeText(this, "Clicked item two", Toast.LENGTH_SHORT).show()
-            R.id.nav_item_three -> Toast.makeText(this, "Clicked item three", Toast.LENGTH_SHORT).show()
-            R.id.nav_item_four -> Toast.makeText(this, "Clicked item four", Toast.LENGTH_SHORT).show()
+
+            R.id.world -> Toast.makeText(this, "Clicked item two", Toast.LENGTH_SHORT).show()
+            R.id.technology -> Toast.makeText(this, "Clicked item three", Toast.LENGTH_SHORT).show()
+            R.id.culture -> Toast.makeText(this, "Clicked item four", Toast.LENGTH_SHORT).show()
+            R.id.religion -> Toast.makeText(this, "Clicked item four", Toast.LENGTH_SHORT).show()
+            R.id.business -> Toast.makeText(this, "Clicked item four", Toast.LENGTH_SHORT).show()
+            R.id.health -> Toast.makeText(this, "Clicked item four", Toast.LENGTH_SHORT).show()
+            R.id.sports -> Toast.makeText(this, "Clicked item four", Toast.LENGTH_SHORT).show()
         }
         drawer.closeDrawer(GravityCompat.START)
         return true
